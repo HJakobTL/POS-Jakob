45be2ecb4f22f0cd301006d4d737f9ab4f0724d8
EST2374 <jack.cez2004@gmail.com>
2025-10-09 20:38:35 +0200

    Quadrat_fertig

---
+++ Einsteigerkurs/quadrat.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class quadrat here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class quadrat
+{
+    public static void quadrat(int x)
+    {
+        for (int zeile = 0; zeile < x; zeile++){
+            for (int spalte = 0; spalte < x; spalte++){
+                if (zeile == 0 || zeile == x-1){
+                    System.out.print('#');
+                }
+                else if (zeile != spalte && spalte != 0 && spalte != x-1){
+                    System.out.print(" ");
+                }
+                else System.out.print('#');
+            }
+            System.out.print("\n");
+        }
+    }
+
+}
