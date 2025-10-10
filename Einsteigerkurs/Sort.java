737ed5a68e3d0889a04c6e20d877167df3c9321b
EST2374 <jack.cez2004@gmail.com>
2025-10-10 16:02:48 +0200

    Namen_maker

--- Einsteigerkurs/Sort.java
+++ Einsteigerkurs/Sort.java
@@ -13,7 +13,7 @@ public class Sort
         int l = arr.length;
         int z;
         for (int i = 0; i < l-1; i++){
-            for (int j = 0; j < l-i-1; i++){
+            for (int j = 0; j < l-i-1; j++){
             if (arr[j+1] > arr[j]){
                 z = arr[j];
                 arr[j] = arr[j+1];
