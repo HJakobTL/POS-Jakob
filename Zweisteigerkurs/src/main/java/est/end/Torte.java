45cbfad5b860c8a48b8cbf966360dcfe729e2e38
EST2374 <jack.cez2004@gmail.com>
2025-11-20 21:53:04 +0100

    Torte add

---
+++ Zweisteigerkurs/src/main/java/est/end/Torte.java
@@ -0,0 +1,39 @@
+package est.end;
+public class Torte
+{
+    public static void hochzeit(int n){
+        String line = "";
+        n *= 2;
+        for (int zeile = 0;  zeile < n/2; zeile++){
+            for(int spalte = 0; spalte < n+1;spalte++){
+                if(spalte == n/2-zeile){
+                    line += "[";
+                }
+                if (spalte == n/2+zeile+1){
+                    line += "]";
+                }
+                if ( zeile % 2 == 0 && spalte == n/2+zeile+1){
+                    line += "¡";
+                }
+                else if ( zeile % 2 == 0 && spalte == n/2-zeile-1){
+                    line += "¡";
+                }
+                else if(spalte <= n/2 && spalte >= n/2-zeile){
+                    line += "*";
+                }
+                else if (spalte >= n/2 && spalte <= n/2+zeile){
+                    line += "*";
+                }
+                else line += " ";
+            }
+            line += "\n";
+        }
+        System.out.print(line);
+    }
+
+    public static void main(String[] args){
+        Torte.hochzeit(5);
+        Torte.hochzeit(6);
+    }
+}
+
