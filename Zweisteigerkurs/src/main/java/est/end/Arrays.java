86eac3b597821582c29ed75d312e22abb2b5682b
EST2374 <jack.cez2004@gmail.com>
2026-01-18 15:31:04 +0100

    Array

---
+++ Zweisteigerkurs/src/main/java/est/end/Arrays.java
@@ -0,0 +1,15 @@
+package est.end;
+
+public class Arrays {
+    static int[] kooridinaten(boolean[][] matrix){
+        //Muss durch den Array iterieren um die Stelle zu finden
+        //bei dem TRUE steht
+        return new int[]{1,2,3};
+    }
+
+    static int indexValue (int gesucht, int[] array) {
+        //bsp.: array {1,2,3,4,5}
+        //ges.: idnex 4
+        return 3;
+    }
+}
