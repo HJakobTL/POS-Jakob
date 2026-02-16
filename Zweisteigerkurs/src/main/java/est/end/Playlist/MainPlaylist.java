c36c7dde70f6949c1e1174b38b58e6364a16ca13
EST2374 <jack.cez2004@gmail.com>
2026-02-16 23:11:05 +0100

    sync 2.0 lol

--- Zweisteigerkurs/src/main/java/est/end/Playlist/MainPlaylist.java
+++ Zweisteigerkurs/src/main/java/est/end/Playlist/MainPlaylist.java
@@ -13,26 +13,26 @@ public class MainPlaylist {
         playlist1.songHinzufuegen(s2);
         playlist1.songHinzufuegen(s3);
         playlist1.songHinzufuegen(s4);
-        /*
+
         playlist1.songHinzufuegen(s1);
         playlist1.songHinzufuegen(s1);
         playlist1.songHinzufuegen(s1);
         playlist1.songHinzufuegen(s2);
         playlist1.songHinzufuegen(s2);
         playlist1.songHinzufuegen(s1);
-        */
+
         // Filter Genre new Array Test
-        //System.out.println(playlist1.filterGenre(""));
+        System.out.println(playlist1.filterGenre("Rock"));
         // Pre Remove
         System.out.println(playlist1);
         // s1 sollte nicht removed werden
-        //playlist1.removeDuplicat(s1);
-        //playlist1.removeDuplicat(s2);
+        playlist1.removeDuplicat(s1);
+        playlist1.removeDuplicat(s2);
         // After Remove
         System.out.println(playlist1);
 
         //Shuffle
-        playlist1.shufflePlaylist();
+        //playlist1.shufflePlaylist();
 
 
     }
